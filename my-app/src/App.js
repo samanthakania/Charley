39f91f851f357f1ef4c7ab45fda376ecec8060a3
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-03-28 23:06:48 +0000

    added some seeds to the database and successfully fetched from the rails data base and console loged it on the reactDOM

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  
+  
+  componentDidMount() {
+    this.fetchData();
+  }
+  
+  fetchData() {
+    fetch('/parks/index')
+    .then(response => {return response.json()
+    })
+    // .then(parsedJSON => console.log(parsedJSON.results))
+    .then(data => console.log(data))
+    .catch(err => console.log('parsing failed', err))
+
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <h1>hit hit</h1>
+      
         </header>
       </div>
     );
