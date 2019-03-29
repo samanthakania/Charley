168bb4ad70de3ff028f8f07dbbd3ea0227b5e09b
Ryan Andrew Sibat <ryanandrew@Ryan-mbp.local>
2019-03-29 16:36:16 -0400

    created seperate components for different view tabs

---
+++ my-app/src/components/ModalWindowComponents/SocialMediaTab.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SocialMediaTab extends Component {
+    render() {
+        return(
+            <div className="socialMediaContainer">
+                <header>
+                    <h1>Social Media Window Tab</h1>
+                </header>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur rerum omnis repudiandae natus? Excepturi corrupti laboriosam fugiat libero aspernatur. A aut eveniet voluptatum delectus perferendis, consequatur eum nam quasi!</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SocialMediaTab;
