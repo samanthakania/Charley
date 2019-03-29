82efc793616202c525d58f94c5dd7f4a4da99d83
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-03-29 14:44:11 +0000

    added all parks to seeds deleted the useless page controller created a model for maps and cleaned up routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MyMapComponent from './components/MyMapComponent.jsx';
 import NavBar from './components/NavBar';
 
 class App extends Component {
-  
-  
-  
+
+
+
   componentDidMount() {
     this.fetchData();
   }
-  
+
   fetchData() {
     fetch('/parks/index')
     .then(response => {return response.json()
@@ -21,13 +20,20 @@ class App extends Component {
     .catch(err => console.log('parsing failed', err))
 
   }
-  
+
   render() {
     return (
       <div className="App">
+        < NavBar />
+        < MyMapComponent
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyADfvn7bqN8wTxIrPUpCZZMEoURzEsj5sU"
+          containerElement={<div className="mapContainer"/>}
+          loadingElement={<div className="loadingElement"/>}
+          mapElement={<div className="mapElement"/>}
+        />
         <header className="App-header">
       <h1>hit hit</h1>
-      
+
         </header>
       </div>
     );
