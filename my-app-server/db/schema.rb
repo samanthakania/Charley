2fef4f5070bae79ea33422111eba9a9ac10428e6
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-03-29 14:35:41 +0000

    added all parks to database seeds, refactored previous seeds,added a weather_info collum and designation colum created a routes model for db deleted pages controller

--- my-app-server/db/schema.rb
+++ my-app-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_182301) do
+ActiveRecord::Schema.define(version: 2019_03_29_035840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_03_28_182301) do
     t.string "activites"
     t.integer "established"
     t.boolean "unesco"
+    t.string "designation"
+    t.text "weather_info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
