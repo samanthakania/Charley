d7f6755bcefd59c52bf7aa1c044e34404cad2848
Ryan Andrew Sibat <ryanandrew@Ryan-mbp.local>
2019-03-30 12:14:33 -0400

    added general temp contentent and social media tab and added component to modal window

--- my-app/src/components/ModalWindowComponents/SocialMediaTab.jsx
+++ my-app/src/components/ModalWindowComponents/SocialMediaTab.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import './ModalWindowComponents.css';
 
 class SocialMediaTab extends Component {
     render() {
         return(
-            <div className="socialMediaContainer">
-                <header>
-                    <h1>Social Media Window Tab</h1>
-                </header>
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur rerum omnis repudiandae natus? Excepturi corrupti laboriosam fugiat libero aspernatur. A aut eveniet voluptatum delectus perferendis, consequatur eum nam quasi!</p>
+            <div className="social-media-container">
+            <div className="hero-image">
+                <div className="hero-text">
+                    <h1>Example National Park</h1>
+                    <p>This park is great</p>  
+                    <button>Add Park</button>
                 </div>
             </div>
+            <div className="social-media-content">
+                <span className="likes"> 2,000 likes</span> 
+                <div className="comments">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti eveniet provident saepe ut minima omnis nam mollitia aperiam cumque, numquam a obcaecati earum eius autem aliquam temporibus, eos fugit perferendis.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolores non unde ratione eius modi asperiores, error dignissimos architecto? Magni deleniti amet, laboriosam totam distinctio laudantium quaerat autem fugit fuga.</p>
+
+                </div>
+            </div>
+        </div>
         )
     }
 }
