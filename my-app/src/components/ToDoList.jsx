3837fae67e77821e24588caff4e430003451750e
Ryan Andrew Sibat <ryanandrew@Ryan-mbp.local>
2019-04-01 15:51:43 -0400

    completed basic todo list

--- my-app/src/components/ToDoList.jsx
+++ my-app/src/components/ToDoList.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import './ToDoList.css'
 
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo, removeTodo }) {
+    return(
+        <div 
+        className="todo"
+        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        > 
+            {todo.text} 
+            <div>
+                <button onClick={ () => completeTodo(index) } > Complete </button>
+                <button onClick={() => removeTodo(index)}>x</button>
+            </div>
+        </div>
+    )
+}
 
-function ToDoForm({ addTodo }) {
+function TodoForm({ addTodo }) {
     const [value, setValue] = useState("");
 
     const handleSubmit = e => {
@@ -24,22 +37,52 @@ function ToDoForm({ addTodo }) {
 }
 
 function ToDoList() {
+
     const[todos, setTodos] = useState([
-        { text: "Learn about React" },
-        { text: "Meet friend for lunch" },
-        { text: "Build really cool todo app" }
-    ])
+        { 
+            text: "Learn about React",
+            isCompleted: false
+        },
+        { 
+            text: "Meet friend for lunch",
+            isCompleted: true
+        },
+        { 
+            text: "Build really cool todo app",
+            isCompleted: true
+        }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos);
+    };
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
+    };
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
 
     return (
         <div className="app">
             <div className="todo-list">
                 {todos.map((todo, index) => (
                     <Todo
-                        key={index}
-                        index={index}
-                        todo={todo}
+                        key = { index }
+                        index = { index }
+                        todo = { todo }
+                        completeTodo = { completeTodo }
+                        removeTodo = { removeTodo }
                     />
                 ))}
+                <TodoForm addTodo={addTodo} /> 
             </div>
         </div>
     );
