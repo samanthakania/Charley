be104e2d9c097310461ac1fa8b13b328e59baecb
Ryan Andrew Sibat <ryanandrew@Ryan-mbp.local>
2019-04-02 10:22:18 -0400

    created basic todo list

--- my-app/src/components/ToDoList.jsx
+++ my-app/src/components/ToDoList.jsx
@@ -10,7 +10,7 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
             {todo.text} 
             <div>
                 <button onClick={ () => completeTodo(index) } > Complete </button>
-                <button onClick={() => removeTodo(index)}>x</button>
+                <button onClick={ () => removeTodo(index) }> x </button>
             </div>
         </div>
     )
