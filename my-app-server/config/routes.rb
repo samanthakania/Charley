026ba5e75b92e8319dbd28cd6195a395c0195d72
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-04-02 11:45:39 -0400

    added four apis to get information about parks from the national parks service, photos from flickr, tweets from twiiter and campsite informations form tail api deleted ALL keys and saved them in git ignores and env keys must be added to endpoints before information will be returned

--- my-app-server/config/routes.rb
+++ my-app-server/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'api/nps_api_show'
   get 'parks/index'
+  post '/park/show' => 'parks#show'
+  get 'twitterfeed/index'
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
