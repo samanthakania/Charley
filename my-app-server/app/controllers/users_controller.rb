533115026d293a4393fea05c3053ff503ba77081
Samantha Kania <samantha.kania12@gmail.com>
2019-04-04 15:17:59 +0000

    mailer setup

--- my-app-server/app/controllers/users_controller.rb
+++ my-app-server/app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
    def create
      # Create the id from params
-     @tripid = User.new(params[:trip_id])
-     if @user.save
+     @trip_id = Trip.new(trip_params)
+     if @trip_id.save
        # Deliver the email
        MapMailer.send_signup_email(@trip_id).deliver
        redirect_to(@trip_id, :notice => 'Trip created')
@@ -10,4 +10,9 @@ class UsersController < ApplicationController
        render :action => 'new'
      end
    end
+     private
+
+  def trip_params
+    params.require(:trip).permit(:trip_id, :email)
+  end
  end
--- my-app-server/app/mailers/map_mailer.rb
+++ my-app-server/app/mailers/map_mailer.rb
@@ -1,9 +1,9 @@
 class MapMailer < ApplicationMailer::Base
   default :from => 'charley@example.com'
 
-  def send_signup_email(tripid)
-    @tripid = tripid
-    mail( :to => @tripid.email,
+  def send_signup_email(trip_id)
+    @trip_id = trip_id
+    mail( :to => @trip_id.email,
     :subject => 'Thank you for creating a trip' )
     end
   end
