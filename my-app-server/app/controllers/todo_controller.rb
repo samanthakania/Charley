ce75f49c459d61eb2b4e8bd9dd7ce0e16329ac8d
Ryan Andrew Sibat <ryanandrew@Ryan-mbp.local>
2019-04-04 17:06:58 -0400

    removed debugging code

--- my-app-server/app/controllers/todo_controller.rb
+++ my-app-server/app/controllers/todo_controller.rb
@@ -4,7 +4,6 @@ class TodoController < ApplicationController
         puts "********************"
         puts "test: #{params[:text]}"
         puts "********************"
-        byebug
     end
 
 end
