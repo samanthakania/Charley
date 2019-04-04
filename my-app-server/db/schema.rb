83e9508147dcce218c291e2c170bf6ab0649cfbe
Samantha Kania <samantha.kania12@gmail.com>
2019-04-04 15:33:27 +0000

    db

--- my-app-server/db/schema.rb
+++ my-app-server/db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_04_04_141206) do
   create_table "trips", force: :cascade do |t|
     t.string "trip_id"
     t.string "email"
-    t.integer "park_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
